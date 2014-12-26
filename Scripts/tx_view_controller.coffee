c9513020e03478afda5e56e3097c9e389e8c69ff
Stuart Binder <sbinder@stdesign.com>
2014-12-26 14:35:34 -0500

    Added deduction logic to TX Center

--- Scripts/tx_view_controller.coffee
+++ Scripts/tx_view_controller.coffee
@@ -1,7 +1,14 @@
-class @TXViewController
+class TXViewController
    API: 'http://localhost:54870/'
+   transaction: 0
+   balance: 0   
+   direction: -1
 
    constructor: ->
+      $('body').keydown (event)=>
+         if $('div#GiftCenter').is(':visible') 
+            @GotTXKey(event)
+         
       #$('div#GiftCenter').hide()
       if CookieJar.xread()
          window.location.replace 'index.html'
@@ -32,9 +39,12 @@ class @TXViewController
    gotAccount: (data) ->
       @HideAll()
       #$('div#TXCenter').hide()
-      $('div#current span.val').html(@Money(data.giftbal))
-      $('div#new span.val').html(@Money(data.giftbal))
-      $('div#amount').html(@Money(0))
+      @balance = data.giftbal
+      @transaction = 0
+      $('div#current span.val').html(@Money(@balance))
+      @ShowTX()
+      #$('div#new span.val').html(@Money(data.giftbal))
+      #$('div#amount').html(@Money(0))
       $('div#GiftCenter').fadeIn(500)
       
    LookupTel: ->
@@ -50,8 +60,34 @@ class @TXViewController
          cents = '0' + mons if mons.length == 1
       else
          dols = mons.substring(0,mons.length - 2)
+         dols = '0' if dols.length == 0
          cents = mons.substring(mons.length - 2)
       return "$" + dols + "." + cents
+
+   ShowTX: =>
+      $('div#amount').html(@Money(@transaction))
+      $('div#new span.val').html(@Money(@balance + (@transaction * @direction)))
+
+   GotTXKey: (event) =>
+      oldTX = @transaction
+      regex = new RegExp("^[0-9]+$")
+      str = String.fromCharCode(event.which)
+      if regex.test(str)
+         @transaction *= 10
+         @transaction += parseInt(str);
+      if event.which == 8 || event.which == 46
+         ts = '' + @transaction
+         if ts.length > 1          
+            @transaction = parseInt(ts.substring(0, -1 + ts.length))         
+         else 
+            @transaction = 0
+      if @balance + (@transaction * @direction) < 0
+         @transaction = oldTX         
+      @ShowTX()      
+      #return true
+      #event.preventDefault()
+      #return false
+
 
 $ ->
    new TXViewController
--- Scripts/tx_view_controller.js
+++ Scripts/tx_view_controller.js
@@ -1,8 +1,26 @@
 (function() {
-  this.TXViewController = (function() {
+  var TXViewController,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  TXViewController = (function() {
     TXViewController.prototype.API = 'http://localhost:54870/';
 
+    TXViewController.prototype.transaction = 0;
+
+    TXViewController.prototype.balance = 0;
+
+    TXViewController.prototype.direction = -1;
+
     function TXViewController() {
+      this.GotTXKey = __bind(this.GotTXKey, this);
+      this.ShowTX = __bind(this.ShowTX, this);
+      $('body').keydown((function(_this) {
+        return function(event) {
+          if ($('div#GiftCenter').is(':visible')) {
+            return _this.GotTXKey(event);
+          }
+        };
+      })(this));
       if (CookieJar.xread()) {
         window.location.replace('index.html');
       }
@@ -49,9 +67,10 @@
 
     TXViewController.prototype.gotAccount = function(data) {
       this.HideAll();
-      $('div#current span.val').html(this.Money(data.giftbal));
-      $('div#new span.val').html(this.Money(data.giftbal));
-      $('div#amount').html(this.Money(0));
+      this.balance = data.giftbal;
+      this.transaction = 0;
+      $('div#current span.val').html(this.Money(this.balance));
+      this.ShowTX();
       return $('div#GiftCenter').fadeIn(500);
     };
 
@@ -72,9 +91,40 @@
         }
       } else {
         dols = mons.substring(0, mons.length - 2);
+        if (dols.length === 0) {
+          dols = '0';
+        }
         cents = mons.substring(mons.length - 2);
       }
       return "$" + dols + "." + cents;
+    };
+
+    TXViewController.prototype.ShowTX = function() {
+      $('div#amount').html(this.Money(this.transaction));
+      return $('div#new span.val').html(this.Money(this.balance + (this.transaction * this.direction)));
+    };
+
+    TXViewController.prototype.GotTXKey = function(event) {
+      var oldTX, regex, str, ts;
+      oldTX = this.transaction;
+      regex = new RegExp("^[0-9]+$");
+      str = String.fromCharCode(event.which);
+      if (regex.test(str)) {
+        this.transaction *= 10;
+        this.transaction += parseInt(str);
+      }
+      if (event.which === 8 || event.which === 46) {
+        ts = '' + this.transaction;
+        if (ts.length > 1) {
+          this.transaction = parseInt(ts.substring(0, -1 + ts.length));
+        } else {
+          this.transaction = 0;
+        }
+      }
+      if (this.balance + (this.transaction * this.direction) < 0) {
+        this.transaction = oldTX;
+      }
+      return this.ShowTX();
     };
 
     return TXViewController;
